99ece2c54a75a7d714993c777df66905b931a8a5
yeyeye <h2012497638@outlook.com>
2022-10-02 17:04:03 +0800

    添加了通过id搜索用户方法

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,6 +1,13 @@
 package dao;
 
-public interface UserDao {
+import pojo.po.User;
 
+public interface UserDao {
+    /**
+     * 通过用户id获取该用户
+     * @param userId 需要查找的用户id
+     * @return 存在则返回该用户pojo，不存在返回null
+     */
+    User selectUserById(int userId);
 
 }
